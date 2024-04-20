182fa5cca4615402985698399300ba338e979676
xy-gt_ghvfvv <pawapareek506@gmail.com>
2024-04-20 16:18:34 +0530

    db config update

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.log('DB connection error = ', error)
+    console.log('DB connection error:', error);
   }
 };
 
-export default connectDB
+export default connectDB;
